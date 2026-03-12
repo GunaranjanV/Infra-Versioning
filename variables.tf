0c83d9dec51375456fe5264e9f1197c30bade5de
GunaranjanV <gunaachar003@gmail.com>
2026-03-12 12:33:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "ec2_instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "bucket_name" {
